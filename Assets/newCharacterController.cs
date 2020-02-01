2994a10330fda3095111f5ee25c79a56f966ab73
Enmanuel Salcedo <manuel.salcedo.rd@gmail.com>
2020-02-01 15:34:37 -0400

    cambios
    modelos y codigo

---
+++ Assets/newCharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newCharacterController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float moveSpeed;
+    public float Gravity = 7.9f;
+    public float jumpSpeed = 5;
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection.x = Input.GetAxis("Horizontal") * moveSpeed;
+
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0;
+
+            //moveDirection *= moveSpeed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                moveDirection.y = jumpSpeed;
+
+        }
+
+
+        moveDirection.y -= Gravity * Time.deltaTime;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
