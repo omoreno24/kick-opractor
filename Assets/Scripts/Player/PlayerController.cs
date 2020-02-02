7214641bbb39073f1fd680cd6e2f4dd24cbbdac0
Oswald Moreno <oswald.akira@gmail.com>
2020-02-02 13:29:48 -0400

    animation fix, screenshake

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,8 @@
 using Player.Movement;
 using System;
 using UnityEditor.Animations;
+using UnityEngine.UI;
+using System.Collections;
 
 namespace Player
 {
@@ -10,29 +12,63 @@ namespace Player
         public Animator animator;
         public PlayerProperties properties;
         IPlayerLocomotion movementController;
+        bool isPunching = false;
+        public int counter = 0;
+        public Text CounterText;
+        public GameObject hitbox;
+        public bool CanPunch;
 
         void Start()
         {
             movementController = gameObject.GetComponent<IPlayerLocomotion>();
+            CanPunch = true;
         }
 
         void Update()
         {
+            CounterText.text = "Backpain fix Count: " + counter;
+
+            if (isPunching == true && (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || animator.GetCurrentAnimatorStateInfo(0).IsName("Running"))) { hitbox.SetActive(false); isPunching = false; animator.ResetTrigger("punch"); animator.SetBool("Punch", false); }
+
+
             Vector3 motion = new Vector3(Input.GetAxis(properties.HorizontalInput), 0, Input.GetAxis(properties.VerticalInput));
+            var running = motion != Vector3.zero;
 
             if (Input.GetButton(properties.LightHitButton) || Input.GetButton(properties.HeavyHitButton))
             {
                 motion = Vector3.zero;
+                running = false;
+
+                if(CanPunch) StartCoroutine(Punch());
             }
 
-            var running = motion != Vector3.zero;
+
             animator.SetBool("Running", running);
 
             if (running && !properties.FX.isPlaying) properties.FX.PlayOneShot(properties.WalkClip, 0.2f);
-           
-            movementController.Move(motion);
+
+            if(!isPunching)
+                movementController.Move(motion);
+            else
+                movementController.Move(motion * 0.2f);
 
             CheckPlayerDeath();
+        }
+
+        public IEnumerator Punch()
+        {
+            CanPunch = false;
+            ResetPunch();
+            animator.SetTrigger("punch");
+            //animator.SetBool("Punch", true);
+            animator.SetBool("Running", false);
+
+            hitbox.SetActive(true);
+            isPunching = true;
+
+            yield return new WaitForSeconds(0.2f);
+
+            CanPunch = true;
         }
 
         private void CheckPlayerDeath()
@@ -44,6 +80,11 @@ namespace Player
         {
             Debug.Log("Player Death");
             shaker.Shake(0.7f, 4);
+        }
+
+        public void ResetPunch()
+        {
+            animator.SetBool("Punch", false);
         }
     }
 }
---
+++ Assets/enemyTroop.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+public class enemyTroop : MonoHelper
+{
+    public int health = 100;
+    public bool canBeHurt = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //GRAVITY
+        //transform.position -= new Vector3(0, 2 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerHitbox" && canBeHurt)
+        {
+            shaker.ShakeOneShotDirectional(player.transform.position - transform.position,0.1f);
+            health -= 45;
+            canBeHurt = false;
+            Invoke("ReHurt", 0.6f);
+            var newPoss = (other.transform.position - transform.position) * 0.3f;
+            
+            transform.position -= newPoss;  //Vector3.Lerp(transform.position, transform.position - (other.transform.position - transform.position) * 1, Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, -1, transform.position.z);
+        }
+
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+            FindObjectOfType<PlayerController>().counter++;
+        }
+
+    }
+
+    void ReHurt()
+    {
+        canBeHurt = true;
+    }
+}
